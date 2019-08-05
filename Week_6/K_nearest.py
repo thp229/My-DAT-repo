227a0b61088267c310430fec46e5abf49fa4bdb6
Theodore Plotkin <theodoreplotkin@Theodores-MBP-2.home>
2019-08-05 01:38:51 -0400

    initial commit

--- Week_6/K_nearest.py
+++ Week_6/K_nearest.py
@@ -4,19 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random as r
 
-#randomly generated 1000 rows with a qualitative response
-data = {"X1":[r.gauss(0,1) for i in range(500)]+[r.gauss(0,1) for i in range(500)],
-        "X2":[r.uniform(0,5) for i in range(500)]+[r.uniform(5,10) for i in range(500)],
-        "Y":[0 for i in range(500)] + [1 for i in range (500)]
-}
-X = pd.DataFrame.from_dict(data) 
-
-def knn(k,target, data):
+def kNN(k,target, data):
     prediction = None
     T = list(data.loc[target, :][:2])
 
     data["index"] = data.index.tolist()
-    Remainder = data.loc[data.index != target, X.columns != "Y"]
+    Remainder = data.loc[data.index != target, data.columns != "Y"]
     
     rankings = []
     for i in range(len(Remainder)):
@@ -54,23 +47,29 @@ def knn(k,target, data):
 
 
 ##########################################################
-target = 234
+#randomly generated 1000 rows with a qualitative response
+X = pd.DataFrame.from_dict({"X1":[r.gauss(0,1) for i in range(500)]+[r.gauss(0,1) for i in range(500)],
+        "X2":[r.uniform(0,5) for i in range(500)]+[r.uniform(5,10) for i in range(500)],
+        "Y":[0 for i in range(500)] + [1 for i in range (500)]
+}) 
 
-print("target row:", list(X.loc[target, :]))
-neighbors_plot, prediction = knn(100,target,X)
-if prediction == list(X.loc[target, :])[2]:
-    print("The KNN was correct!")
-else:
-    print("The KNN was incorrect")
 
-def viewPlots():
-    fig , ax = plt.subplots(1,2, sharey = False, figsize = (14,5))
-    sns.scatterplot(x  = "X1", y = "X2", hue = "category", data = neighbors_plot, ax = ax[0])
-    sns.scatterplot(x  = "X1", y = "X2", hue = "Y", data = neighbors_plot, ax = ax[1])
-    ax[0].title.set_text("Subset of 100 nearest neighbors to target value")
-    ax[1].title.set_text("The decision boundary and target value"+"\n"+"The KNN predicted: "+str(prediction))
-    plt.scatter(x=list(X.loc[target, :])[0], y=list(X.loc[target, :])[1], color='r')
-    plt.show()
+#target = 234
+for target in X.index.tolist():
+    
+    neighbors_plot, prediction = kNN(100,target,X)
 
-viewPlots()
+    def viewPlots():
+        fig , ax = plt.subplots(1,2, sharey = False, figsize = (14,5))
+        sns.scatterplot(x  = "X1", y = "X2", hue = "category", data = neighbors_plot, ax = ax[0])
+        sns.scatterplot(x  = "X1", y = "X2", hue = "Y", data = neighbors_plot, ax = ax[1])
+        ax[0].title.set_text("Subset of 100 nearest neighbors to target value")
+        if prediction == list(X.loc[target, :])[2]:
+            ax[1].title.set_text("The decision boundary and target value"+"\n"+"The KNN predicted: "+str(prediction)+"\n"+"The KNN was correct!")
+        else:
+            ax[1].title.set_text("The decision boundary and target value"+"\n"+"The KNN predicted: "+str(prediction)+"\n"+"The KNN was incorrect!")
+        plt.scatter(x=list(X.loc[target, :])[0], y=list(X.loc[target, :])[1], color='r')
+        plt.show()
+
+    viewPlots()
 
